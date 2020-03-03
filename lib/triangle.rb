dd03fea262cca337b1c2df83a7d5fe1400803fd8
Megan Horsfield <meganthadev@gmail.com>
2020-03-03 06:13:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,44 +1,33 @@
 class Triangle
 
-  attr_accessor :x, :y, :z
+  attr_accessor :side1, :side2, :side3
   
-  def initialize(x, y, z)
-    @triangle_sides = []
-    @triangle_sides << x
-    @triangle_sides << y
-    @triangle_sides << z
-  end
+  def initialize(side1, side2, side3)
+    @side1 = side1 
+    @side2 = side2
+    @side3 = side3
+  end 
   
-  def valid?
-    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
-    
-    if (@triangle_sides.none? {|side| side <= 0}) &&
-      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
-      return true
-    else
-      return false
-    end
-  end
-  
-    
- def kind
-    if valid?
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
-      else
-        return :scalene
-      end
-    else
+  def kind
+    if (side1 <= 0) || (side2 <= 0) || (side3 <= 0)
       raise TriangleError
-      end
+    elsif (side1 + side2 <= side3) || (side1 + side3 <= side2) || (side2 + side3 <= side1)
+      raise TriangleError
+    else 
+      if (side1 == side2) && (side2 == side3)
+        :equilateral
+      elsif (side1 == side2) || (side2 == side3) || (side1 == side3)
+        :isosceles
+      elsif (side1 != side2) && (side2 != side3) && (side1 != side3)
+        :scalene
+      end 
     end
-  end
+  end 
+
 
 class TriangleError < StandardError
+end
+
 end
 
 
